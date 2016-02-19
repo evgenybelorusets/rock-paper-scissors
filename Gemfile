38fa985fa8ea1d8ed6c144965414a67f9a2819a3
Evgeny Belosets <evgenybelorusets@gmail.com>
2016-02-19 20:44:02 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.5.1'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+
+
+group :development, :test do
+  gem 'byebug'
+  gem 'rspec-rails'
+end
---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require File.expand_path('../boot', __FILE__)
+
+require "action_controller/railtie"
+require "sprockets/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RockPaperScissors
+  class Application < Rails::Application
+  end
+end
